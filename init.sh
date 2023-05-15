65fd8581ebe23db6709adfd269580f002d1b0f09
Dragos Cojocari <dragos.cojocari@snyk.io>
2023-05-15 12:06:55 +0300

    chore-test-secrets

---
+++ init.sh
@@ -0,0 +1,8 @@
+export AWS_ACCESS_KEY_ID=yLryKGwcGc3ez9G8YAnjeYMQOc # Informative, can't be used w/o the secret key
+export AWS_SECRET_ACCESS_KEY=nAH2VzKrMrRjySLlt8HCdFU3tM2TUuUZgh39NX
+export AWS_DEFAULT_REGION='us-west-1' # Broad enough that it doesn't create risk by itself.
+export AWS_REGION=$AWS_DEFAULT_REGION
+export S3_REGION=$AWS_DEFAULT_REGION
+export S3_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export encryptedUsername="MDIEEPgAAAAAAAAAAAAAAAAAAAEwFAYIKoZIhvcNAwcECF7kv84cNrhKBAgHD6N4RU01Tg=="
+export encryptedPassword="MDoEEPgAAAAAAAAAAAAAAAAAAAEwFAYIKoZIhvcNAwcECBUufYeWbuziBBAraNDREdVus+piXPZaR/Ym"
